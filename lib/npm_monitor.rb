d9ce0195b9e4fbf844f1bedd9ee1cf0e38f3bbe2
Christian Joudrey <cmallette@gmail.com>
2012-07-01 00:26:03 -0400

    Added NpmMonitor#format_package to format the npm package into rubygem-like object.

--- lib/npm_monitor.rb
+++ lib/npm_monitor.rb
@@ -26,4 +26,39 @@ class NpmMonitor
     body['changes']
   end
 
+  def format_package(package)
+    latest = package['versions'][package['dist-tags']['latest']]
+
+    formatted = {
+      :authors          => latest['author']['name'],
+      :dependencies     => {
+        :development    => [],
+        :runtime        => []
+      },
+      :dependencies     => {
+        :development    => [],
+        :runtime        => []
+      },
+      :info             => latest['description'],
+      :name             => latest['name'],
+      :version          => latest['version']
+    }
+
+    (latest['devDependencies'] || {}).each_key.sort.each do |name|
+      formatted[:dependencies][:development] << {
+        :name           => name,
+        :requirements   => latest['devDependencies'][name]
+      }
+    end
+
+    (latest['dependencies'] || {}).each_key.sort.each do |name|
+      formatted[:dependencies][:runtime] << {
+        :name           => name,
+        :requirements   => latest['dependencies'][name]
+      }
+    end
+
+    formatted
+  end
+
 end
--- test/npm_monitor.rb
+++ test/npm_monitor.rb
@@ -48,4 +48,87 @@ class NpmMonitorTest < Test::Unit::TestCase
     assert_equal 'another-package', changes[1]['id']
   end
 
+  def test_format_package
+    package = {
+      '_id'         => 'express',
+      '_rev'        => '302-c61077032ef8b66dccd3b6e94294528a',
+      'name'        => 'express',
+      'description' => 'Sinatra inspired web development framework',
+      'dist-tags'   => {
+        '3.0'       => '3.0.0beta3',
+        'latest'    => '2.5.11'
+      },
+      'versions'    => {
+        '2.5.11'      => {
+          'name'         => 'express',
+          'description'  => 'Sinatra inspired web development framework',
+          'version'      => '2.5.11',
+          'author'       => {
+            'name'         => 'TJ Holowaychuk',
+            'email'        => 'tj@vision-media.ca'
+          },
+          'contributors' => [],
+          'dependencies' => {
+            'connect'      => '>= 1.4.0 < 2.0.0',
+            'mime'         => '>= 0.0.1',
+            'qs'           => '>= 0.0.6'
+          },
+          'devDependencies' => {
+            'expresso'        => '0.7.2',
+          },
+          'keywords'     => [],
+          'repository'   => {
+              'type'       => 'git',
+              'url'        => 'git://github.com/visionmedia/express.git'
+          },
+          'main'         => 'index',
+          'bin'          => {
+              'express'    => './bin/express'
+          },
+          'engines'     => {
+              'node'      => '>= 0.4.1 < 0.5.0'
+          },
+          '_id'              =>  'express@2.3.5',
+          '_engineSupported' => true,
+          '_npmVersion'      => '1.0.3',
+          '_nodeVersion'     => 'v0.4.7',
+          '_defaultsLoaded'  => true,
+          'dist'             => {
+              'shasum'         => 'a3113d0d9db4ea118e2c12b044a04c16741e799b',
+              'tarball'        => 'http://registry.npmjs.org/express/-/express-2.3.5.tgz'
+          },
+          'scripts'          => {},
+          'directories'      => {}
+        }
+      }
+    }
+
+    formatted = @monitor.format_package(package)
+
+    expected = {
+      :authors                => 'TJ Holowaychuk',
+      :dependencies           => {
+        :development          => [{
+          :name               => 'expresso',
+          :requirements       => '0.7.2'
+        }],
+        :runtime              => [{
+          :name               => 'connect',
+          :requirements       => '>= 1.4.0 < 2.0.0'
+        }, {
+          :name               => 'mime',
+          :requirements       => '>= 0.0.1'
+        }, {
+          :name               => 'qs',
+          :requirements       => '>= 0.0.6'
+        }]
+      },
+      :info                   => 'Sinatra inspired web development framework',
+      :name                   => 'express',
+      :version                => '2.5.11'
+    }
+
+    assert_equal expected, formatted
+  end
+
 end
