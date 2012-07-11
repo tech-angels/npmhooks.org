b46808a183249972d55a0184176966ecb04dcb75
Christian Joudrey <cmallette@gmail.com>
2012-07-10 20:55:22 -0400

    Generate a random unique key whenever a ApiKey is creasted.

---
+++ test/unit/api_key_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class ApiKeyTest < ActiveSupport::TestCase
+
+  def test_random_key
+    key = ApiKey.random_key
+    assert_equal 32, key.length
+
+    key2 = ApiKey.random_key
+    assert_not_equal key, key2
+  end
+
+  def test_random_unique_key
+    expected = '*' * 32
+    ApiKey.expects(:random_key).times(2).returns(api_keys(:cjoudrey_key).api_key, expected)
+    assert_equal expected, ApiKey.random_unique_key
+  end
+
+  def test_create
+    key = ApiKey.new
+    key.user = users(:cjoudrey)
+    key.save
+    assert_not_nil key.api_key
+  end
+
+end
