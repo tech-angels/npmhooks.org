32ec60f24f99b95c263306cb5364f3293dddd5b3
Christian Joudrey <cmallette@gmail.com>
2012-07-10 21:07:05 -0400

    Automatically assigning a user an api key on create.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
   has_many :web_hooks
   has_many :api_keys
 
+  after_create :auto_assign_api_key
+
+  def auto_assign_api_key
+    self.api_keys << ApiKey.create
+  end
+
   def self.from_omniauth(auth)
     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
   end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_auto_assign_api_key_on_create
+    user = User.create({
+      :provider => 'github',
+      :uid      => '1234',
+      :login    => '1234'
+    }, :without_protection => true)
+
+    assert_not_nil user.api_keys
+  end
+
+  def test_auto_assign_api_key_only_on_create
+    users(:cjoudrey).save
+    assert_equal 1, users(:cjoudrey).api_keys.length
+  end
 end
