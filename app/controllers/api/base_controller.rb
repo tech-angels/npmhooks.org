4f7f5516267e150da5d33c7363246a92386be71c
Christian Joudrey <cmallette@gmail.com>
2012-07-14 17:53:03 -0400

    Refactoring API base controller

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,15 +1,18 @@
 class Api::BaseController < ApplicationController
-  before_filter :authenticate_with_api_key
   before_filter :verify_authenticated_user
 
-  def authenticate_with_api_key
-    api_key = request.headers['Authorization'] || params[:api_key]
-    @current_user = User.find_by_api_key(api_key)
-  end
-
+  private
   def verify_authenticated_user
-    if @current_user.nil?
+    if current_user.nil?
       render :text => 'Access Denied. Please sign up for an account at https://npmhooks.org', :status => 401
     end
+  end
+
+  def request_api_key
+    request.headers['Authorization'] || params[:api_key]
+  end
+
+  def current_user
+    @current_user = User.find_by_api_key(request_api_key)
   end
 end
--- app/controllers/api/v1/web_hooks_controller.rb
+++ app/controllers/api/v1/web_hooks_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::WebHooksController < Api::BaseController
   respond_to :json, :only => :index
 
   def index
-    respond_with @current_user.web_hooks
+    respond_with current_user.web_hooks
   end
 
   def create
-    webhook = @current_user.web_hooks.build(:url => params[:url])
+    webhook = current_user.web_hooks.build(:url => params[:url])
 
     if webhook.save
       render(:text => webhook.success_message, :status => :created)
@@ -16,7 +16,7 @@ class Api::V1::WebHooksController < Api::BaseController
   end
 
   def remove
-    webhook = @current_user.web_hooks.find_by_url(params[:url])
+    webhook = current_user.web_hooks.find_by_url(params[:url])
     if webhook.try(:destroy)
       render(:text => webhook.removed_message, :status => 200)
     else
