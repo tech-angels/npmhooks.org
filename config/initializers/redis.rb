91ef65a6c47df5d7b71a6f99ac94ef18ae87a5b9
Philippe Lafoucrière <philippe.lafoucriere@gmail.com>
2012-11-21 15:31:46 +0100

    Fix redis initilizer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,7 @@ redis_connection = Redis.new({
   :port     => ENV['REDIS_PORT'] || 6379,
   :password => ENV['REDIS_PASSWORD'] || nil,
   :timeout  => ENV['REDIS_TIMEOUT'] || 5.0,
-  :database => ENV['REDIS_DATABASE'] || 0,
+  :db       => ENV['REDIS_DATABASE'] || 0,
 })
 
 Redis.current = redis_connection
